50b26fcde5d180ef58e9868afbaf8d2283154c3d
Takuya Noguchi <takuya@netlab.jp>
2016-03-18 09:22:10 +0900

    JSONをパースして予約を取得するようにコードを修正。

--- slack.rb
+++ slack.rb
@@ -5,39 +5,61 @@ CALENDAR_URL = ENV['CALENDAR_URL']
 require 'net/http'
 require 'uri'
 require 'json'
-require 'date'
-
-require_relative './html_parser'
+require 'time'
 
 class Slack
-  def self.post(text)
-    data = { "text" => text }
-    request_url = ROOM_URL
-    uri = URI.parse(request_url)
-    Net::HTTP.post_form(uri, {"payload" => data.to_json})
-  end
+  class << self
+    def tweet_resevations
+      reservations = parse_json
+      today = Time.now.strftime('%Y/%m/%d')
 
-  def self.tweet_resevations
-    reservations = HTMLParser.reservations
-    today = Date.today.strftime('%Y/%m/%d')
-
-    tweet_text = if reservations.empty?
-                   <<-EOS
+      tweet_text = if reservations.empty?
+                     <<-EOS
 本日(#{today})は会議室予約がありません。
 
 #{CALENDAR_URL}
                    EOS
-                 else
-                   <<-EOS
+                   else
+                     <<-EOS
 本日(#{today})の会議室予約状況です。
 
 #{reservations.join("\n")}
 
 #{CALENDAR_URL}
                    EOS
-                 end
+                   end
 
-    post(tweet_text)
+      post(tweet_text)
+    end
+
+
+    private
+    def get_json
+      uri = URI.join(URI.parse(CALENDAR_URL), '/calendar/reservations')
+      Net::HTTP.get(uri)
+    end
+
+    def parse_json
+      parsed_json = JSON.parse(get_json)
+      reservations = []
+
+      parsed_json.each do |reservation|
+        start_time = Time.parse(reservation['start']).strftime('%H:%M')
+        end_time = Time.parse(reservation['end']).strftime('%H:%M')
+        reservation_time = "#{start_time}-#{end_time}"
+        title_and_room = "#{reservation['title']}  #{reservation['room']}"
+        reservations << "#{reservation_time}  #{title_and_room}"
+      end
+
+      reservations
+    end
+
+    def post(text)
+      data = { "text" => text }
+      request_url = ROOM_URL
+      uri = URI.parse(request_url)
+      Net::HTTP.post_form(uri, { "payload" => data.to_json })
+    end
   end
 end
 
